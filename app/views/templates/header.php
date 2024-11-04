b89795f2a6e73e76259d18c1ba0be06f8ea9837c
Chandra13July <chandra13july2004@gmail.com>
2024-11-04 13:45:22 +0700

    Update Fitur Favicon

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,5 +5,5 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cafe Bonanza</title>
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="<?= BASEURL; ?>/img/favicon/icon.png" type="image/x-icon">
 </head>
